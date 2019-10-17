dca5cd421b3d64cc7151769459f33d585e862b6e
sadoubalde-esa <54898403+sadoubalde-esa@users.noreply.github.com>
2019-10-17 13:44:12 +0200

    Add files via upload

---
+++ helper.R
@@ -0,0 +1,61 @@
+
+install.packages("xlsx") 
+install.packages("dplyr") 
+install.packages("lubridate") 
+install.packages("tidyr") 
+install.packages("stringr") 
+
+library(xlsx)
+library(dplyr)
+library(lubridate)
+library(tidyr)
+library(stringr)
+
+# Read & Manipulate Data -------------------------------------------------------
+read.data <- function() {
+  
+  data<- read.xlsx2("./train.xlsx", sheetIndex = 1,
+                         colClasses = c("numeric", "numeric", 
+                                        "numeric","numeric", 
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric","numeric",
+                                        "numeric")) 
+  data
+}
+  
+               
+read.test <- function() {  
+  
+                    test<- read.xlsx2("./test.xlsx", sheetIndex = 1,
+                                      colClasses = c("numeric", "numeric", 
+                                                     "numeric","numeric", 
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric","numeric",
+                                                     "numeric"))
+test
+                    
+}
+                        
+ 
